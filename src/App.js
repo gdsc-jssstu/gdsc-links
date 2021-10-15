a8b4a7833e130216db137d6d620161a6bb6eafcd
Poorvi <65484904+PoorviShetty@users.noreply.github.com>
2021-10-15 11:20:01 +0530

    very basic theme toggling :")

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import "./App.css";
+import './App.css';
 import DisplayLinks from "./containers/DisplayLinks";
 import Navbar from "./containers/Navbar";
+import React, { useState } from 'react';
 
+ 
 function App() {
+  const [mode, setMode] = useState('light'); 
+
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#111';
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+    }
+  }
   return (
     <>
-      <Navbar />
-      <DisplayLinks />
-    </>
+      <Navbar mode={mode} toggleMode={toggleMode}/>
+      <DisplayLinks mode={mode}/>
+    </> 
   );
 }
 
