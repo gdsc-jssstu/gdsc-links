77618f392e13096e12557e8a934905866072e83f
Alaap Surendran <alaapsurendran@gmail.com>
2021-10-16 01:00:42 +0530

    store theme in local storage to persist it

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
 import './App.css';
 import DisplayLinks from "./containers/DisplayLinks";
 import Navbar from "./containers/Navbar";
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Confetti from 'react-confetti'
-
  
 function App() {
   const [mode, setMode] = useState('light'); 
 
-  const toggleMode = ()=>{
-    if(mode === 'light'){
+  const setTheme = (theme) => {
+    if ('light' === theme) {
+      setMode('light');
+      window.localStorage.setItem('theme', 'light');
+      document.body.style.backgroundColor = 'white';
+    } else if ('dark' === theme) {
       setMode('dark');
+      window.localStorage.setItem('theme', 'dark');
       document.body.style.backgroundColor = '#111';
     }
-    else{
-      setMode('light');
-      document.body.style.backgroundColor = 'white';
+  };
+
+  const toggleMode = () => {
+    setTheme(mode === 'light'? 'dark' : 'light');
+  };
+
+  useEffect(() => {
+    const localTheme = window.localStorage.getItem('theme');
+    if (localTheme) {
+      setTheme(localTheme);
+    } else {
+      window.localStorage.setItem('theme', 'light');
+      setTheme('light');
     }
-  }
+  }, []);
+
   return (
     <>
       <Confetti recycle={false} numberOfPieces={200} />
