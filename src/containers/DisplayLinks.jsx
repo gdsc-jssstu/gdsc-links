44f405919c39508588c14ff2367ee59bed3ad877
Abhilash S Hathwar <76783449+Abhilash437@users.noreply.github.com>
2023-03-11 00:48:19 +0530

    Update DisplayLinks.jsx

--- src/containers/DisplayLinks.jsx
+++ src/containers/DisplayLinks.jsx
@@ -9,7 +9,7 @@ function DisplayLinks() {
   const [error, setError] = useState(null); 
  
   useEffect(async () => { 
-    const foo = await axios(process.env.REACT_APP_LINKS_DATA) 
+    const foo = await axios("https://api.npoint.io/18ff90d12cad1df9272d") 
       .then((response) => { 
         setData(response.data); 
         console.log(response.data);
